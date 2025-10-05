73724720062c332e5bb0305732c49a59fd9abfbd
marciobever <bevervansomarcio@gmail.com>
2025-10-05 22:36:03 +0100

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-// server component por padrão
-import { redirect } from 'next/navigation';
-
-export default function Index() {
-  redirect('/'); // se o seu landing estiver em /(site)/page.tsx e for a rota raiz,
-                 // use redirect('/home') se você colocar em /home
-}
