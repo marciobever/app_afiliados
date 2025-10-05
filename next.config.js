824c49c03b3323c5bde026b3e50b45a0fdf8bcfe
marciobever <bevervansomarcio@gmail.com>
2025-10-05 17:57:18 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // importante p/ imagem leve
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      // evita tentar empacotar pg-native (não usamos)
+      config.externals.push('pg-native');
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
