44e3fd87fd8969259e21c8885eca2696e5626203
marciobever <bevervansomarcio@gmail.com>
2025-10-06 14:59:20 +0100

    Update route.ts

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,14 +1,11 @@
 // app/api/auth/logout/route.ts
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { NextResponse } from "next/server";
 import { clearSessionCookie } from "@/lib/auth";
 
 export async function POST() {
-  const res = NextResponse.json({ ok: true });
-  clearSessionCookie(res as any);
-  return res;
-}
-
-export async function GET() {
   const res = NextResponse.json({ ok: true });
   clearSessionCookie(res as any);
   return res;
